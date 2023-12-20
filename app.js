56ac2f701e985a986381a4699ec49f64e8b95b2f
Fauze Cavalari <fauze.cavalari@gmail.com>
2023-12-19 21:08:34 -0300

    updating changes with nodemon

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+//requesting express
 const express = require('express')
+
+//instace app with express
+const app = express();
+
+//get domain root / with req & res
+app.get('/', (req, res) =>{
+    //res.send => return an html
+    res.send('<h1>My task list</h1>')
+})
+
+//init server, listen 3000
+app.listen(3000, () =>{
+    //return when server is online
+    console.log('Server initialized')
+})
+
+//install nodemon => monitoring files, if modified restart server
+//to see changes
+
+//run => npx nodemon 
+
